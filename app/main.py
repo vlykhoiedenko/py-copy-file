7de395345d240a16686baebdbe55ea0402cd2edf
Vladislav Lykhoiedenko <v.lykhoiedenko@gmail.com>
2025-10-18 13:17:09 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-def copy_file(command: str):
+def copy_file(command: str) -> None:
     parts = command.split()
     if len(parts) == 3 and parts[0] == "cp":
         source_file_name, target_file_name = parts[1], parts[2]
